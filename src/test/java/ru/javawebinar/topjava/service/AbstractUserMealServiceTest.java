6c77aee57174324bdb8b47a3f73f7a38fafa93a4
ilnur serbaev <serbaevilnur@gmail.com>
2016-04-09 17:31:45 +0500

    HW5 tests remove duplicate field log

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserMealServiceTest.java
@@ -5,8 +5,6 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.Description;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.UserMeal;
@@ -22,7 +20,6 @@ import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 abstract public class AbstractUserMealServiceTest extends AbstractServiceTest {
-    private static final Logger LOG = LoggerFactory.getLogger(AbstractUserMealServiceTest.class);
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -31,7 +28,7 @@ abstract public class AbstractUserMealServiceTest extends AbstractServiceTest {
     // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
     public Stopwatch stopwatch = new Stopwatch() {
         private void logInfo(Description description, long nanos) {
-            LOG.info(String.format("+++ Test %s spent %d ms",
+            log.info(String.format("+++ Test %s spent %d ms",
                     description.getMethodName(), TimeUnit.NANOSECONDS.toMillis(nanos)));
         }
 
