8b571a19c2c785f80aa0c12cb1e9019aedb08328
ilnur serbaev <serbaevilnur@gmail.com>
2016-05-29 21:05:15 +0500

    7-10-rest-test-jackson

--- src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
@@ -1,8 +1,13 @@
 package ru.javawebinar.topjava.model;
 import javax.persistence.*;
 
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 @MappedSuperclass
 @Access(AccessType.FIELD)
+@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
 public class BaseEntity {
     public static final int START_SEQ = 100000;
 
