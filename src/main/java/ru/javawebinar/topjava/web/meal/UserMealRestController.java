c723a8c457494b8d073ed2c2870ad9e8a8646772
ilnur serbaev <serbaevilnur@gmail.com>
2016-06-07 23:14:51 +0500

    8-03-HW07-formatter

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
@@ -2,18 +2,19 @@ package ru.javawebinar.topjava.web.meal;
 
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.to.UserMealWithExceed;
+import ru.javawebinar.topjava.util.TimeUtil;
 
 @RestController
 @RequestMapping(value = UserMealRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -48,10 +49,7 @@ public class UserMealRestController extends AbstractUserMealController {
                 .path(REST_URL + "/{id}")
                 .buildAndExpand(created.getId()).toUri();
 
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(uriOfNewResource);
-
-        return new ResponseEntity<>(created, httpHeaders, HttpStatus.CREATED);
+        return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
     @RequestMapping(value = "/between", method = RequestMethod.GET)
@@ -59,5 +57,14 @@ public class UserMealRestController extends AbstractUserMealController {
             @RequestParam(value = "startDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDateTime,
             @RequestParam(value = "endDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDateTime) {
         return super.getBetween(startDateTime.toLocalDate(), startDateTime.toLocalTime(), endDateTime.toLocalDate(), endDateTime.toLocalTime());
+    }
+
+    @RequestMapping(value = "/filter", method = RequestMethod.GET)
+    public List<UserMealWithExceed> getBetween(
+            @RequestParam(value = "startDate", required = false) LocalDate startDate, @RequestParam(value = "startTime", required = false) LocalTime startTime,
+            @RequestParam(value = "endDate", required = false) LocalDate endDate, @RequestParam(value = "endTime", required = false) LocalTime endTime) {
+        return super.getBetween(
+                startDate != null ? startDate : TimeUtil.MIN_DATE, startTime != null ? startTime : LocalTime.MIN,
+                endDate != null ? endDate : TimeUtil.MAX_DATE, endTime != null ? endTime : LocalTime.MAX);
     }
 }
