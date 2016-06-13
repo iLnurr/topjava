2d3cf9d3fb9bf50c090e8715f6dbd8cfb7696c5c
ilnur serbaev <serbaevilnur@gmail.com>
2016-06-13 13:08:37 +0500

    10-11-password-encoding

--- src/test/java/ru/javawebinar/topjava/UserTestData.java
+++ src/test/java/ru/javawebinar/topjava/UserTestData.java
@@ -1,8 +1,11 @@
 package ru.javawebinar.topjava;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.util.PasswordUtil;
 import ru.javawebinar.topjava.util.UserUtil;
 
 import java.util.EnumSet;
@@ -12,6 +15,8 @@ import java.util.Set;
 import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
 
 public class UserTestData {
+    private static final Logger LOG = LoggerFactory.getLogger(UserTestData.class);
+
     public static final int USER_ID = START_SEQ;
     public static final int ADMIN_ID = START_SEQ + 1;
 
@@ -35,6 +40,7 @@ public class UserTestData {
         }
 
         public User asUser() {
+            return UserUtil.prepareToSave(new User(this));
             return new User(this.getId(), this.getName(), this.getEmail(), this.getPassword(), this.getCaloriesPerDay(),
                     this.isEnabled(), this.getRoles());
         }
@@ -61,7 +67,7 @@ public class UserTestData {
             }
 
             TestUser that = (TestUser) o;
-            return Objects.equals(this.password, that.password)
+            return comparePassword(this.password, that.password)
                     && Objects.equals(this.id, that.id)
                     && Objects.equals(this.name, that.name)
                     && Objects.equals(this.email, that.email)
@@ -69,5 +75,15 @@ public class UserTestData {
                     && Objects.equals(this.enabled, that.enabled)
                     && Objects.equals(this.roles, that.roles);
         }
+    }
+
+    private static boolean comparePassword(String rawPassword, String password) {
+        if (PasswordUtil.isEncoded(rawPassword)) {
+            LOG.warn("Expected password couldn't be compared with actual");
+        } else if (!PasswordUtil.isMatch(rawPassword, password)) {
+            LOG.error("Password " + password + " doesn't match encoded " + password);
+            return false;
+        }
+        return true;
     }
 }
