29321158cf11567e7bf8d33d8a47dfd0b57335f5
ilnur Serbaev <serbaevilnur@gmail.com>
2016-06-20 16:05:07 +0500

    Update ErrorInfo.java

--- src/main/java/ru/javawebinar/topjava/util/exception/ErrorInfo.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/ErrorInfo.java
@@ -1,9 +1,5 @@
 package ru.javawebinar.topjava.util.exception;
 
-/**
- * User: gkislin
- * Date: 19.08.2014
- */
 public class ErrorInfo {
     public final String url;
     public final String cause;
